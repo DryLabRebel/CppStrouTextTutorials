1279fc410d67db9c919eb8b65be676e6bfbdf284
Geoff English <geoffrey.english@uqconnect.edu.au>
2019-01-24 05:27:44 +1000

    small changes

--- helloprogramming.cpp
+++ helloprogramming.cpp
@@ -4,8 +4,8 @@
 
 int main() // C++ programs start by executing the function 'main'
 {
-    cout << "Hello, programming!\n"; // Standard output 'cout' "Hello, programming!"
-    cout << "Here we go!\n";
+    cout << "\n\nHello, programming!\n\n"; // Standard output 'cout' "Hello, programming!"
+    cout << "Here we go!\n\n";
     keep_window_open(); // wait for a character to be entered
     return 0;
 }
